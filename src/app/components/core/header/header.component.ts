315e8b4118bf06c05e8174e370f76af23c1bc54e
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-12-21 13:01:54 +0200

    set styles to the active route in navbar

--- src/app/components/core/header/header.component.ts
+++ src/app/components/core/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component, NgModule } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
 })
 export class HeaderComponent {}
