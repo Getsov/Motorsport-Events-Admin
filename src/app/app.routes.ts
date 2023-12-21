2d923eae999a4c8b016420083a9a520121af9c09
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-12-21 12:33:32 +0200

    add url routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AwaitingEventsComponent } from './components/events/awaiting-events/awaiting-events.component';
+import { EventsComponent } from './components/events/events/events.component';
+import { CreateEventComponent } from './components/events/create-event/create-event.component';
+import { AccountsComponent } from './components/accounts/accounts/accounts.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'awaiting-events', component: AwaitingEventsComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'create-event', component: CreateEventComponent },
+  { path: 'accounts', component: AccountsComponent },
+  { path: 'profile', component: ProfileComponent },
+];
