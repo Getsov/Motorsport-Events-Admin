80f3aa222ab44151e476ca1e7ed2e1e5d6ae5f86
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-12-21 13:46:48 +0200

    update architecture and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { AwaitingEventsComponent } from './components/events/awaiting-events/awaiting-events.component';
 import { EventsComponent } from './components/events/events/events.component';
 import { CreateEventComponent } from './components/events/create-event/create-event.component';
 import { AccountsComponent } from './components/accounts/accounts/accounts.component';
 import { ProfileComponent } from './components/user/profile/profile.component';
+import { AwaitingApprovalComponent } from './components/events/awaiting-approval/awaiting-approval.component';
 
 export const routes: Routes = [
-  { path: 'awaiting-events', component: AwaitingEventsComponent },
+  { path: 'awaiting-approval', component: AwaitingApprovalComponent },
   { path: 'events', component: EventsComponent },
   { path: 'create-event', component: CreateEventComponent },
   { path: 'accounts', component: AccountsComponent },
---
+++ src/app/components/events/awaiting-approval/awaiting-approval.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-awaiting-approval',
+  standalone: true,
+  imports: [],
+  templateUrl: './awaiting-approval.component.html',
+  styleUrl: './awaiting-approval.component.scss'
+})
+export class AwaitingApprovalComponent {
+
+}
