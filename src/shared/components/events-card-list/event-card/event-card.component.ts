9cc3b707ebd71d3f896ef81a31b35cb2897525fd
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-04 09:27:39 +0200

    add event-card with basic css

--- src/shared/components/events-card-list/event-card/event-card.component.ts
+++ src/shared/components/events-card-list/event-card/event-card.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Event } from '../../../interfaces/Event';
+import { CommonModule, IMAGE_CONFIG } from '@angular/common';
 
 @Component({
   selector: 'app-event-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './event-card.component.html',
-  styleUrl: './event-card.component.scss'
+  styleUrl: './event-card.component.scss',
 })
 export class EventCardComponent {
-
+  @Input() event: Event = {
+    shortTitle: '',
+    shortDescription: '',
+    longDescription: '',
+    visitorPrices: [],
+    dates: [],
+    imageUrl: '',
+    contacts: {
+      coordinates: {
+        lat: 0,
+        long: 0,
+      },
+      region: '',
+      address: '',
+      phone: '',
+      email: '',
+    },
+    category: '',
+    likes: [],
+    creator: '',
+    isDeleted: false,
+    _id: '',
+    isApproved: false,
+  };
 }
--- src/shared/components/events-card-list/events-card-list.component.ts
+++ src/shared/components/events-card-list/events-card-list.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Event } from '../../interfaces/Event';
+import { EventCardComponent } from './event-card/event-card.component';
 
 @Component({
   selector: 'app-events-card-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EventCardComponent],
   templateUrl: './events-card-list.component.html',
   styleUrl: './events-card-list.component.scss',
 })
 export class EventsCardListComponent {
-  @Input() list: any = [];
+  @Input() eventsList: Event[] = [];
 }
