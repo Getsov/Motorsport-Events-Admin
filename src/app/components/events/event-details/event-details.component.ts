231aff60a0211c961ea656d32db757cc077b8e44
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-05 14:31:13 +0200

    fix cards styling, add details component

---
+++ src/app/components/events/event-details/event-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-details.component.html',
+  styleUrl: './event-details.component.scss'
+})
+export class EventDetailsComponent {
+
+}
