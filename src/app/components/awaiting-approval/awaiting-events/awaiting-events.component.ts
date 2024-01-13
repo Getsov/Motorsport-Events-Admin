b20c01c77bd2b1c63e14a9e69ea05dbad6e90828
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-13 15:11:35 +0200

    add loader, test it in awaiting-events

--- src/app/components/awaiting-approval/awaiting-events/awaiting-events.component.ts
+++ src/app/components/awaiting-approval/awaiting-events/awaiting-events.component.ts
@@ -1,21 +1,30 @@
-import { Component, effect } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventsCardListComponent } from '../../../../shared/components/events-card-list/events-card-list.component';
 import { EventService } from '../../../../shared/services/event.service';
 import { Event } from '../../../../shared/interfaces/Event';
+import { LoaderComponent } from '../../../../shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-awaiting-events',
   standalone: true,
-  imports: [EventsCardListComponent],
+  imports: [EventsCardListComponent, LoaderComponent],
   templateUrl: './awaiting-events.component.html',
-  styleUrl: './awaiting-events.component.scss',
+  styleUrls: ['./awaiting-events.component.scss'],
 })
-export class AwaitingEventsComponent {
-  awaitingApprovalEvents: any = [];
+export class AwaitingEventsComponent implements OnInit {
+  awaitingApprovalEvents: Event[] = [];
+  isLoading: boolean = true;
 
-  constructor(private eventService: EventService) {
-    effect(() => {
-      this.awaitingApprovalEvents = this.eventService.eventsForApproval();
-    });
+  constructor(private eventService: EventService) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.effect();
+    }, 2000);
+  }
+
+  effect() {
+    this.awaitingApprovalEvents = this.eventService.eventsForApproval();
+    this.isLoading = false;
   }
 }
