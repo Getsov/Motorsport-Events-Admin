98858b52ee6a241df32aaee948a1e08d71bcaecf
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-14 13:28:49 +0200

    add html & css to awaiting-admins

--- src/app/components/awaiting-approval/awaiting-admins/awaiting-admins.component.ts
+++ src/app/components/awaiting-approval/awaiting-admins/awaiting-admins.component.ts
@@ -2,11 +2,13 @@ import { Component, effect } from '@angular/core';
 import { User } from '../../../../shared/interfaces/User';
 import { EventService } from '../../../../shared/services/event.service';
 import { UserService } from '../../../../shared/services/user.service';
+import { LoaderComponent } from '../../../../shared/components/loader/loader.component';
+import { AccountsTableComponent } from '../../../../shared/components/accounts-table/accounts-table.component';
 
 @Component({
   selector: 'app-awaiting-admins',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent, AccountsTableComponent],
   templateUrl: './awaiting-admins.component.html',
   styleUrl: './awaiting-admins.component.scss',
 })
