c791321fc34f3574865666d28ff09046590c18f3
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-18 19:27:01 +0200

    load fresh data then click in option at section-nav

--- src/app/components/awaiting-approval/awaiting-organizers/awaiting-organizers.component.ts
+++ src/app/components/awaiting-approval/awaiting-organizers/awaiting-organizers.component.ts
@@ -1,8 +1,9 @@
-import { Component, effect } from '@angular/core';
+import { Component, OnDestroy, OnInit, effect } from '@angular/core';
 import { UserService } from '../../../../shared/services/user.service';
 import { User } from '../../../../shared/interfaces/User';
 import { AccountsTableComponent } from '../../../../shared/components/accounts-table/accounts-table.component';
 import { LoaderComponent } from '../../../../shared/components/loader/loader.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-awaiting-organizers',
@@ -11,15 +12,23 @@ import { LoaderComponent } from '../../../../shared/components/loader/loader.com
   templateUrl: './awaiting-organizers.component.html',
   styleUrl: './awaiting-organizers.component.scss',
 })
-export class AwaitingOrganizersComponent {
+export class AwaitingOrganizersComponent implements OnDestroy {
   awaitingApprovalOrganizers: User[] = [];
   isloading: boolean = true;
+  subscription: Subscription | undefined;
 
   constructor(private userService: UserService) {
+    this.subscription = this.userService.setOrganizersForApprove();
     effect(() => {
       this.awaitingApprovalOrganizers =
         this.userService.organizersForApproval();
-      this.isloading = true;
+      this.isloading = false;
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
