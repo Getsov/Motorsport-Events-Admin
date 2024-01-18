205fcb9b135aec05bfd707ab0f06be41f5850fc9
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-18 23:13:30 +0200

    add html & css and handle submit in login form

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  onLoginSubmit(form: NgForm) {
+    console.log('submit');
+    this.router.navigate(['/']);
+  }
 
+  constructor(private router: Router) {}
 }
