93929f5a16827b5851e3828aaa2477f16017c7bf
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-21 16:51:05 +0200

    add user guard

---
+++ src/shared/guards/user.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable, effect } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    if (!!this.authService.userDetails.accessToken) {
+      return this.router.navigateByUrl('/');
+    } else {
+      return true;
+    }
+  }
+}
