21b58c2959457f89f866478b80c893d5d48b6dfb
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-22 14:28:12 +0200

    create organizator registration propetries

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { LabelWithStatesComponent } from '../../../../shared/components/label-with-states/label-with-states.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [LabelWithStatesComponent, CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-
+  registerData = {
+    email: '',
+    password: '',
+    rePassword: '',
+    firstName: '',
+    phoner: '',
+    region: '',
+    role: 'organizer',
+  };
 }
--- src/shared/guards/user.guard.ts
+++ src/shared/guards/user.guard.ts
@@ -24,13 +24,9 @@ export class UserGuard implements CanActivate {
     | Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree> {
     if (!!this.authService.userDetails.accessToken) {
-      console.log('hop false');
-
       this.router.navigateByUrl('/user/login');
       return false;
     } else {
-      console.log(' hop true');
-
       return true;
     }
   }
