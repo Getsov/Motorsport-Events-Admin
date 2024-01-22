a409c9d391e0e531b32d13350ddf3f38b521789e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-01-23 01:02:32 +0200

    Made not approwed works
    Fetsch data from DB and render it. If no data, render message.

--- src/app/components/organizer-events/not-approved/not-approved.component.ts
+++ src/app/components/organizer-events/not-approved/not-approved.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SectionSortComponent } from '../../../../shared/components/section-sort/section-sort.component';
+import { AuthService } from '../../../../shared/services/auth.service';
+import { EventService } from '../../../../shared/services/event.service';
+import { Event } from '../../../../shared/interfaces/Event';
+import { LoaderComponent } from "../../../../shared/components/loader/loader.component";
+import { EventsCardListComponent } from "../../../../shared/components/events-card-list/events-card-list.component";
 
 @Component({
-  selector: 'app-not-approved',
-  standalone: true,
-  imports: [],
-  templateUrl: './not-approved.component.html',
-  styleUrl: './not-approved.component.scss'
+    selector: 'app-not-approved',
+    standalone: true,
+    templateUrl: './not-approved.component.html',
+    styleUrl: './not-approved.component.scss',
+    imports: [SectionSortComponent, LoaderComponent, EventsCardListComponent]
 })
-export class NotApprovedComponent {
+export class NotApprovedComponent implements OnInit, OnDestroy {
+  eventsData: Event[] = [];
+  query: any = [];
+  isLoading: boolean = true;
 
+  private eventsSubscription: Subscription = new Subscription();
+
+  constructor(
+    private eventService: EventService,
+    private authService: AuthService
+  ) {}
+
+    ngOnInit(): void {
+        this.getEvents();
+    }
+
+
+  getEvents(): void {
+    this.eventsSubscription = this.eventService.getEventsForApproval().subscribe({
+      next: (events: Event[]) => {
+        this.isLoading = false;
+        this.eventsData = events;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.error(err);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.eventsSubscription) {
+        this.eventsSubscription.unsubscribe();
+      }
+  }
 }
