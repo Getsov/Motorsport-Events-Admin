681bdd0dff39e76330ff006883746ff2a8eeac84
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-26 19:39:36 +0200

    create component for single selector

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 import { LabelWithStatesComponent } from '../../../../shared/components/label-with-states/label-with-states.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SelectorComponent } from '../../../../shared/components/selector/section-sort.component';
-import {
-  IonSelect,
-  IonSelectOption,
-  IonToolbar,
-} from '@ionic/angular/standalone';
+import { SingleSelectorComponent } from '../../../../shared/components/single-selector/single-selector.component';
 
 @Component({
   selector: 'app-register',
@@ -16,7 +11,7 @@ import {
     LabelWithStatesComponent,
     CommonModule,
     FormsModule,
-    SelectorComponent,
+    SingleSelectorComponent,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
---
+++ src/shared/components/single-selector/single-selector.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import BulgarianRegions from '../../data/regions';
+import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-single-selector',
+  templateUrl: './single-selector.component.html',
+  standalone: true,
+  imports: [NgFor, FormsModule, IonSelect, IonSelectOption, CommonModule],
+  styleUrls: ['./single-selector.component.scss'],
+})
+export class SingleSelectorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  regions: any = Object.keys(BulgarianRegions).filter((value) =>
+    isNaN(Number(value))
+  );
+  selectedRegion: string = '';
+
+  onRegionChange(region: string) {
+    this.selectedRegion = region;
+    console.log(this.selectedRegion);
+  }
+}
