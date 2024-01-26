db9e6a807d892a0a9a43e58fcd62de47fd25d703
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-26 11:51:59 +0200

    setup

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { LabelWithStatesComponent } from '../../../../shared/components/label-with-states/label-with-states.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SelectorComponent } from '../../../../shared/components/section-sort copy/section-sort.component';
+import { SelectorComponent } from '../../../../shared/components/selector/section-sort.component';
+import {
+  IonSelect,
+  IonSelectOption,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-register',
--- src/shared/components/selector/section-sort.component.ts
+++ src/shared/components/selector/section-sort.component.ts
@@ -3,11 +3,12 @@ import Categories from '../../data/categories';
 import BulgarianRegions from '../../data/regions';
 import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonSelect, IonSelectOption } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-selector',
   standalone: true,
-  imports: [NgFor, FormsModule],
+  imports: [NgFor, FormsModule, IonSelect, IonSelectOption],
   templateUrl: './selector.component.html',
   styleUrl: './selector.component.scss',
 })
@@ -18,8 +19,14 @@ export class SelectorComponent {
   regions: any = Object.keys(BulgarianRegions).filter((value) =>
     isNaN(Number(value))
   );
+  selectedRegion: string = '';
 
   categories: any = Object.keys(Categories).filter((value) =>
     isNaN(Number(value))
   );
+
+  onRegionChange(region: string) {
+    this.selectedRegion = region;
+    console.log(this.selectedRegion);
+  }
 }
