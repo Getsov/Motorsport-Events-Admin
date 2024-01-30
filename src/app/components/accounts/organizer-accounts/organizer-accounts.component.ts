1be782e6b8a27f8bb5ab2e81450e66e82d90d67f
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-01-30 23:47:50 +0200

    Made organizer accounts works
    Add loading spinner, render organizers from db and made search also works.

--- src/app/components/accounts/organizer-accounts/organizer-accounts.component.ts
+++ src/app/components/accounts/organizer-accounts/organizer-accounts.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, effect } from '@angular/core';
+import { SectionSortComponent } from '../../../../shared/components/section-sort/section-sort.component';
+import { Subscription } from 'rxjs';
+import { User } from '../../../../shared/interfaces/User';
+import { UserService } from '../../../../shared/services/user.service';
+import { LoaderComponent } from '../../../../shared/components/loader/loader.component';
+import { AccountsTableComponent } from '../../../../shared/components/accounts-table/accounts-table.component';
 
 @Component({
   selector: 'app-organizer-accounts',
   standalone: true,
-  imports: [],
   templateUrl: './organizer-accounts.component.html',
-  styleUrl: './organizer-accounts.component.scss'
+  styleUrl: './organizer-accounts.component.scss',
+  imports: [SectionSortComponent, LoaderComponent, AccountsTableComponent],
 })
-export class OrganizerAccountsComponent {
+export class OrganizerAccountsComponent implements OnDestroy {
+  allOrganizers: User[] = [];
+  isLoading: boolean = true;
+  subscription: Subscription | undefined;
 
+  constructor(private userService: UserService) {
+    this.subscription = this.userService.setAllOrganizers();
+    effect(() => {
+      this.allOrganizers = this.userService.allOrganizers();
+      this.isLoading = false;
+    });
+  }
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
