1264ed8d42c11f1bd47be57b821570feda9bf9df
Valeri Getsov <info@vgetsov.com>
2025-02-17 19:29:40 +0200

    refactor: update environment configuration to use environment variables

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,8 +1,15 @@
+declare const process: {
+  env: {
+    GOOGLE_MAPS_API_KEY: string;
+    GOOGLE_MAPS_ANDROID_API_KEY: string;
+    API_BASE_URL: string;
+  };
+};
+
 export const environment = {
   production: false,
   baseUrl: 'https://race-fanatic-server.onrender.com',
-  accessToken:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWE2YTYyZTA0MTIxODA4MjhiYzg4MDAiLCJlbWFpbCI6IkRpbW81OUBhYnYuYmciLCJvcmdhbml6YXRvck5hbWUiOiJCYWkgSXZhbiIsImZpcnN0TmFtZSI6IiIsImxhc3ROYW1lIjoiIiwicm9sZSI6ImFkbWluIiwicmVnaW9uIjoiIiwicGhvbmUiOiIwODk3MzM1NTg3IiwiaXNEZWxldGVkIjpmYWxzZSwiaXNBcHByb3ZlZCI6dHJ1ZSwiaWF0IjoxNzA1NDIwMzc3LCJleHAiOjE3MDYyODQzNzd9.YSVaIkMJ4EgoYuh0hWMA-tc05aeIdSZnj_ixia8rbUo',
-  googleMapsApiKey: 'AIzaSyBZsTJ5gzHoqRJOQONn4ioYWlx7_TqRyXQ',
-  googleMapsAndroidApiKey: '',
+  accessToken: '',
+  googleMapsApiKey: '${GOOGLE_MAPS_API_KEY}',
+  googleMapsAndroidApiKey: '${GOOGLE_MAPS_ANDROID_API_KEY}',
 };
